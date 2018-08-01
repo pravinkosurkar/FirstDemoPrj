98d34439ec6ab42a2c583f88beb784b56ad7c9cf
pravinkosurkar <pravin@123>
2018-08-01 10:58:38 +0530

    Fifth R5

--- app/src/main/java/com/ceinsys/firstdemoprj/NewJavaClass.java
+++ app/src/main/java/com/ceinsys/firstdemoprj/NewJavaClass.java
@@ -1,14 +1,26 @@
 package com.ceinsys.firstdemoprj;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by 05855 on 6/7/2018.
  */
 
-public class NewJavaClass {
-  
-  Log.d("","");
-        Log.d("","kjhfdkjghkgfkjkjfdiuyeirtet");
-  Log.d("","");
-        Log.d("","kjhfdkjghkgfkjkjfdiuyeirtet");
+public class NewJavaClass extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 }
