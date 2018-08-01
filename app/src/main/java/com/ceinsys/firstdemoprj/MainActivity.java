b9c5a7b3ca29aa112b58251bd058f35c09ff91b4
Pravin Kosurkar <pravinkosurkar@gmail.com>
2018-08-01 10:49:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/ceinsys/firstdemoprj/MainActivity.java
+++ app/src/main/java/com/ceinsys/firstdemoprj/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("","");
         Log.d("","kjhfdkjghkgfkjkjfdiuyeirtet");
+        Log.d("","");
     }
 }
